0abf4d309ed8b147b6fdf7caa7135757ba4b56de
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-11-29 12:11:49 +0100

    added error message in forms.js

--- JS/form.js
+++ JS/form.js
@@ -1,13 +1,47 @@
 const formContainer = document.querySelector(`.formContainer`);
-const firstName = document.querySelector(`.firstName`);
-const firstNameError = document.querySelector(`.firstNameError`);
-const lastName = document.querySelector(`.lastName`);
-const lastNameError = document.querySelector(`.lastNameError`);
+const firstName = document.querySelector(`#firstName`);
+const firstNameError = document.querySelector(`#firstNameError`);
+const lastName = document.querySelector(`#lastName`);
+const lastNameError = document.querySelector(`#lastNameError`);
+const email = document.querySelector(`#email`);
+const emailError = document.querySelector(`#emailError`);
 
 function validateForm() {
   event.preventDefault();
+
+  if (checkLength(firstName.value, 0) === true) {
+    firstNameError.style.display = "none";
+  } else {
+    firstNameError.style.display = "block";
+  }
+
+  if (checkLength(lastName.value, 0) === true) {
+    lastNameError.style.display = "none";
+  } else {
+    lastNameError.style.display = "block";
+  }
+
+  if (validateEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
 
   console.log("hello");
 }
 
 formContainer.addEventListener("submit", validateForm);
+
+function checkLength(value, length) {
+  if (value.trim().length > length) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  const regEx = /\S+@\S+\.\S+/;
+  const emailMatch = regEx.test(email);
+  return emailMatch;
+}
