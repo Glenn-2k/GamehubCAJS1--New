a062625683e12fd41a11a01f1b898e56b3d2bc59
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-11-30 13:03:41 +0100

    fixed form

--- JS/form.js
+++ JS/form.js
@@ -7,24 +7,25 @@ const email = document.querySelector(`#email`);
 const emailError = document.querySelector(`#emailError`);
 
 function validateForm() {
-  event.preventDefault();
-
   if (checkLength(firstName.value, 0) === true) {
     firstNameError.style.display = "none";
   } else {
     firstNameError.style.display = "block";
+    event.preventDefault();
   }
 
   if (checkLength(lastName.value, 2) === true) {
     lastNameError.style.display = "none";
   } else {
     lastNameError.style.display = "block";
+    event.preventDefault();
   }
 
   if (validateEmail(email.value) === true) {
     emailError.style.display = "none";
   } else {
     emailError.style.display = "block";
+    event.preventDefault();
   }
 
   console.log("hello");
