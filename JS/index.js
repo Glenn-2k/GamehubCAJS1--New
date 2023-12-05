7314b76fc6fee5483d3454a62d7bb9ce95b3936e
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-12-05 11:48:52 +0100

    made a switch to get the code to work both overall

--- JS/index.js
+++ JS/index.js
@@ -4,10 +4,29 @@ import { getGames } from "../JS/products/games.js";
 import { gamePage } from "./products/game.js";
 import { checked } from "./menu.js";
 
-if (location.pathname === "/games") {
-  gamesPage();
-}
+// if (location.pathname === "/games") {
+//   gamesPage();
+// }
 
-if (location.pathname === "/product-specific.html") {
-  gamePage();
+// if (location.pathname === "/product-specific.html") {
+//   gamePage();
+// }
+
+const cleanedUrl = location.pathname.replace(/\/|\.html$/g, "");
+
+switch (cleanedUrl) {
+  case "games":
+    try {
+      gamesPage();
+    } catch (error) {
+      console.error("error rendering products");
+    }
+    break;
+
+  case "product-specific.html":
+    try {
+      gamePage();
+    } catch (error) {
+      console.error("error rendering products");
+    }
 }
