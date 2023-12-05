affc0318ef3fe68ab479319489ac479f71ac647f
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2023-12-05 11:50:44 +0100

    removed some commented out code

--- JS/index.js
+++ JS/index.js
@@ -4,14 +4,6 @@ import { getGames } from "../JS/products/games.js";
 import { gamePage } from "./products/game.js";
 import { checked } from "./menu.js";
 
-// if (location.pathname === "/games") {
-//   gamesPage();
-// }
-
-// if (location.pathname === "/product-specific.html") {
-//   gamePage();
-// }
-
 const cleanedUrl = location.pathname.replace(/\/|\.html$/g, "");
 
 switch (cleanedUrl) {
